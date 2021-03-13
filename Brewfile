90529ecc0bdceaae84c156c847272140cc5e5ea9
Eric van Wijk <eric@van-wijk.com>
2021-03-13 11:38:29 +0100

    Basic sql function

--- Brewfile
+++ Brewfile
@@ -1,12 +1,15 @@
 tap "azure/functions"
 tap "microsoft/mssql-release"
+tap "isen-ng/dotnet-sdk-versions"
 
 brew "awscli"
 brew "azure-cli"
 brew "azure-functions-core-tools@3"
-#brew "hashicorp/tap/terraform"
+#brew "hashicorp/tap/terraform" # Use tfenv instead
 brew "msodbcsql"
 brew "mssql-tools"
 brew "tfenv"
 
+# cask "dotnet-sdk" # Latest
+# cask "dotnet-sdk3-1-400" # LTS
 cask "powershell"
--- functions/GetRows.cs
+++ functions/GetRows.cs
@@ -1,13 +1,14 @@
+using System;
+using System.Data;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Data;
-using System.Threading.Tasks;
 
 namespace EW.Sql.Function
 {
@@ -19,12 +20,48 @@ namespace EW.Sql.Function
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            int rowsRetrieved = 0;
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                var query = "select top 100 * from dbo.Trip";
+
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    IAsyncResult result = cmd.BeginExecuteReader(CommandBehavior.CloseConnection);
+
+                    int count = 0;
+                    while (!result.IsCompleted)
+                    {
+                        Console.WriteLine("Waiting ({0})", count++);
+                        System.Threading.Thread.Sleep(100);
+                    }
+
+                    using (SqlDataReader reader = cmd.EndExecuteReader(result))
+                    {
+                        while (reader.Read())
+                        {
+                            rowsRetrieved++;
+                            // for (int i = 0; i < reader.FieldCount; i++)
+                            // {
+                            //     Console.Write("{0}\t", reader.GetValue(i));
+                            // }
+                            // Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            log.LogInformation($"{rowsRetrieved} rows were retrieved");
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
